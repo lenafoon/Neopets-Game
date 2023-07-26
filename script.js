050422882f36d2ceb52f7ebe020c619ea7c03e82
lenafoon <lena.foon@gmail.com>
2023-07-26 13:14:57 +0200

    Doughnutfruit Objects added + background image

---
+++ script.js
@@ -0,0 +1,18 @@
+const doughnutfruitsData = [
+    {name: 'Blue-Doughnutfruit' , imageSrc: '', pointValue: 1, isGood: true},
+    {name: 'Green-Doughnutfruit' , imageSrc: '', pointValue: 1, isGood: true},
+    {name: 'Purple-Doughnutfruit' , imageSrc: '', pointValue: 1, isGood: true},
+    {name: 'Yellow-Doughnutfruit' , imageSrc: '', pointValue: 1, isGood: true},
+    {name: 'Grey-Doughnutfruit' , imageSrc: '', pointValue: 1, isGood: true},
+    {name: 'Checkered-Doughnutfruit' , imageSrc: '', pointValue: 2, isGood: true},
+    {name: 'Grilled-Doughnutfruit' , imageSrc: '', pointValue: 2, isGood: true},
+    {name: 'Fish-Doughnutfruit' , imageSrc: '', pointValue: 2, isGood: true},
+    {name: 'Fire-Doughnutfruit' , imageSrc: '', pointValue: 2, isGood: true},
+    {name: 'Ice-Doughnutfruit' , imageSrc: '', pointValue: 2, isGood: true},
+    {name: 'Sponge-Doughnutfruit' , imageSrc: '', pointValue: 2, isGood: true},
+    {name: 'Tropical-Doughnutfruit' , imageSrc: '', pointValue: 2, isGood: true},
+    {name: 'Silver-Doughnutfruit' , imageSrc: '', pointValue: 5, isGood: true},
+    {name: 'Gold-Doughnutfruit' , imageSrc: '', pointValue: 10, isGood: true},
+    {name: 'Rainbow-Doughnutfruit' , imageSrc: '', pointValue: 20, isGood: true}
+
+]
