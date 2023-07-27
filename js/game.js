7ce77f57e0546f5ae005031e46b34f2393f05e81
lenafoon <lena.foon@gmail.com>
2023-07-27 23:29:25 +0200

    doughnutfruit class

--- js/game.js
+++ js/game.js
@@ -7,50 +7,49 @@ const doughnutfruitsData = [
   },
   {
     name: "Green-Doughnutfruit",
-    imageSrc: "/Neopets-Doughnutfruits/foo_doughnutfruit_green.gif",
+    imageSrc: "Neopets-Doughnutfruits/foo_doughnutfruit_green.gif",
     pointValue: 1,
     isGood: true,
   },
   {
     name: "Purple-Doughnutfruit",
-    imageSrc: "/Neopets-Doughnutfruits/foo_doughnutfruit_purple.gif",
+    imageSrc: "Neopets-Doughnutfruits/foo_doughnutfruit_purple.gif",
     pointValue: 1,
     isGood: true,
   },
   {
     name: "Yellow-Doughnutfruit",
-    imageSrc: "/Neopets-Doughnutfruits/foo_doughnutfruit_yellow.gif",
+    imageSrc: "Neopets-Doughnutfruits/foo_doughnutfruit_yellow.gif",
     pointValue: 1,
     isGood: true,
   },
   {
     name: "Grey-Doughnutfruit",
-    imageSrc: "/Neopets-Doughnutfruits/foo_grey_doughnutfruit.gif",
+    imageSrc: "./../Neopets-Doughnutfruits/foo_grey_doughnutfruit.gif",
     pointValue: 1,
     isGood: true,
   },
   {
     name: "Checkered-Doughnutfruit",
     imageSrc: "/Neopets-Doughnutfruits/checkered_doughnutfruit.gif",
-    
     pointValue: 2,
     isGood: true,
   },
   {
     name: "Grilled-Doughnutfruit",
-    imageSrc: "/Neopets-Doughnutfruits/foo_grill_doughnutfruit.gif",
+    imageSrc: "Neopets-Doughnutfruits/foo_grill_doughnutfruit.gif",
     pointValue: 2,
     isGood: true,
   },
   {
     name: "Fish-Doughnutfruit",
-    imageSrc: "/Neopets-Doughnutfruits/foo_doughnutfruit_fish.gif",
+    imageSrc: "Neopets-Doughnutfruits/foo_doughnutfruit_fish.gif",
     pointValue: 2,
     isGood: true,
   },
   {
     name: "Fire-Doughnutfruit",
-    imageSrc: "/Neopets-Doughnutfruits/fire_doughnutfruit.gif",
+    imageSrc: "Neopets-Doughnutfruits/fire_doughnutfruit.gif",
     pointValue: 2,
     isGood: true,
   },
@@ -74,45 +73,56 @@ const doughnutfruitsData = [
   },
   {
     name: "Silver-Doughnutfruit",
-    imageSrc: "/Neopets-Doughnutfruits/foo_doughnutfruit_silver.gif",
+    imageSrc: "Neopets-Doughnutfruits/foo_doughnutfruit_silver.gif",
     pointValue: 5,
     isGood: true,
   },
   {
     name: "Gold-Doughnutfruit",
-    imageSrc: "/Neopets-Doughnutfruits/foo_doughnutfruit_gold.gif",
+    imageSrc: "Neopets-Doughnutfruits/foo_doughnutfruit_gold.gif",
     pointValue: 10,
     isGood: true,
   },
   {
     name: "Rainbow-Doughnutfruit",
-    imageSrc:  "",
+    imageSrc: "Neopets-Doughnutfruitsfoo_doughnutfruit_rainbow.gif",
     pointValue: 20,
     isGood: true,
   },
- 
 ];
+ 
+
+
 
 const dungData = [{ name: "Dung", imageSrc: "Dung.webp", isGood: false }];
 
-class Game {
-  // code to be added
-  constructor() {
-    this.startScreen = document.getElementById("start-screen");
-    this.gameScreen = document.getElementById("game-screen");
-    this.gameEndScreen = document.getElementById("game-over-screen");
-    this.player = new Player(
-      this.gameScreen,
-      200,
-      500,
-      100,
-      150,
-      "./images/car.png"
-    );
-    this.height = 600;
-    this.width = 500;
-    this.obstacles = [];
-    this.score = 0;
-    this.gameIsOver = false;
+/*class Doughnutfruit {
+  constructor(name, imageSrc, pointValue, isGood) {
+    this.name = name;
+    this.imageSrc = imageSrc;
+    this.pointValue = pointValue;
+    this.isGood = isGood;
+    this.speed = 2; 
   }
+}*/
+
+
+class Doughnutfruit {
+  constructor(name, imageSrc, pointValue, isGood) {
+    this.name = name;
+    this.imageSrc = imageSrc;
+    this.pointValue = pointValue;
+    this.isGood = isGood;
+    this.speed = this.calculateSpeed();
+    this.x = Math.random() * 760;
+    this.y = -50;
+    this.element = this.createImageElement();
+  }
+
+  calculateSpeed() {
+
+    return Math.floor(4 / this.pointValue) + 1;
+  }
+
 }
+
